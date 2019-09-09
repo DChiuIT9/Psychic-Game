f4cd63bc1e466b5ba3d48d1b12cf9fd390740472
Daniel Chiu <cycdaniel@gmail.com>
2019-09-09 09:55:46 -0500

    Added reset function.

---
+++ assets/javascript/game-test.js
@@ -0,0 +1,48 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var urGuesses = 0;
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+var urGuessesText = document.getElementById("urguesses-text");
+
+
+// function computerSelection() {
+//     computerGuess;
+// }
+
+// computerSelection();
+
+document.onkeyup = function(event) {
+
+var userGuess = event.key.toLowerCase();
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("Computer: " + computerGuess);
+
+if (userGuess === computerGuess) {
+    wins++;
+    guessesLeft = 9;
+    urGuesses = 0;
+    
+}
+else if (userGuess !== computerGuess) {
+    guessesLeft--
+    urGuesses++
+    if (guessesLeft == 0) {
+        losses++;
+        guessesLeft = 9;
+        urGuesses = 0;
+        
+    }
+}
+    console.log("User: " + userGuess);
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    urGuessesText.textContent = "Your guesses so far: " + urGuesses;
+};
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,37 +3,50 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var urGuesses = 0;
+var userGuesses = [];
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
-var urGuessesText = document.getElementById("urguesses-text");
+var userGuessesText = document.getElementById("userguesses-text");
+
+function userGuessSoFar() {
+    userGuessesText.textContent = "Your guesses so far: " + userGuesses.join(" ");
+}
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("Computer: " + computerGuess);
+
+var reset = function () {
+    userGuesses = [];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("Computer: " + computerGuess);
+}
 
 document.onkeyup = function(event) {
 
 var userGuess = event.key.toLowerCase();
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log("Computer: " + computerGuess);
+userGuesses.push(userGuess);
+userGuessSoFar()
+
     
 if (userGuess === computerGuess) {
     wins++;
     guessesLeft = 9;
-    urGuesses = 0;
+    reset ()
 }
-else if (userGuess !== computerGuess) {
-    guessesLeft--
-    urGuesses++
-    if (guessesLeft == 0) {
-        losses++;
-        guessesLeft = 9;
-        urGuesses = 0;
-    }
+else if (guessesLeft == 0) {
+    losses++;
+    guessesLeft = 9;
+    reset ()
+
 }
+guessesLeft--
+
     console.log("User: " + userGuess);
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-    urGuessesText.textContent = "Your guesses so far: " + urGuesses;
+    userGuessesText.textContent = "Your guesses so far: " + userGuesses.join(" ");
 };
